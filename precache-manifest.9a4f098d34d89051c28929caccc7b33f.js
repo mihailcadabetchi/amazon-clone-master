b93fb56631c65c8a1edb5b1fe516514ebdad2166
Mihail Cadabetchi <74629946+mihailcadabetchi@users.noreply.github.com>
2020-12-11 13:57:54 +0100

    amazon-clone realized with React framework and Firebase for backend
    check here the result for the amazon-clone-website    https://friendly-euclid-b8f7ea.netlify.app

---
+++ precache-manifest.9a4f098d34d89051c28929caccc7b33f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67503738943430377c5b371a9a0db45b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75412d513c5e06334806",
+    "url": "/static/css/main.72d19fe4.chunk.css"
+  },
+  {
+    "revision": "8d24956d353bbaf1a8e8",
+    "url": "/static/js/2.a74c5e59.chunk.js"
+  },
+  {
+    "revision": "9cf9d893b5c51de0aa6dd9e07be64537",
+    "url": "/static/js/2.a74c5e59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75412d513c5e06334806",
+    "url": "/static/js/main.a2807ed6.chunk.js"
+  },
+  {
+    "revision": "6765d93c93b0aa16e2ff",
+    "url": "/static/js/runtime-main.24e2082f.js"
+  }
+]);
